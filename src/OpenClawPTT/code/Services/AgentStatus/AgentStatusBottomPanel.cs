a69de58e71eee1cbe5f6ac81e61e2902da04e61c
Venando <eld.venando@gmail.com>
2026-05-23 17:24:35 +0300

    design: agent bottom panel to show input all the time

--- src/OpenClawPTT/code/Services/AgentStatus/AgentStatusBottomPanel.cs
+++ src/OpenClawPTT/code/Services/AgentStatus/AgentStatusBottomPanel.cs
@@ -97,7 +97,7 @@ public sealed class AgentStatusBottomPanel : IBottomPanel, IDisposable
     public string? CurrentSuggestion => null;
     public bool ShowBottomSeparator => true;
 
-    public bool AllowUserField => !_isSelectionMode || _lastCurrentInput.Length > 0;
+    public bool AllowUserField => true;
 
 
     public IReadOnlyList<string> GetLines(string currentInput)
